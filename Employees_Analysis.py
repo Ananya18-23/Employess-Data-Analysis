404aa7df8a90230132c1c25124b076c16765b14c
Ananya Ginnare <swatigi2004@gmail.com>
2026-04-29 13:22:40 +0530

    Update Employees_Analysis.py

--- Employees_Analysis.py
+++ Employees_Analysis.py
@@ -4,10 +4,10 @@
 # In[1]:
 
 
-# import all  necessary librarires 
+# import all necessary libraries
 
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -15,9 +15,9 @@ import seaborn as sns
 # In[2]:
 
 
-# LOading dataset 
+# Loading dataset
 
-df=pd.read_csv("Employee_data.csv")
+df = pd.read_csv("Employee_data.csv")
 
 
 # In[3]:
@@ -31,7 +31,7 @@ df.head()
 # In[4]:
 
 
-# finding shape of data 
+# finding shape of data
 
 df.shape
 
@@ -39,7 +39,7 @@ df.shape
 # In[5]:
 
 
-# abhi handel nahi keya hai akrna hai 
+# checking duplicates
 df.duplicated().sum()
 
 
@@ -59,7 +59,7 @@ df.duplicated().sum()
 # In[8]:
 
 
-# checking the DataType of each and every column 
+# checking the DataType of each and every column
 
 df.info()
 
@@ -69,7 +69,7 @@ df.info()
 # In[9]:
 
 
-# Checking the null values 
+# Checking the null values
 
 df.isnull().sum()
 
@@ -79,7 +79,7 @@ df.isnull().sum()
 # In[10]:
 
 
-# filling missing value 
+# filling missing value
 
 df['Distance from Home'].fillna(df['Distance from Home'].median(), inplace=True)
 df['Company Tenure (In Months)'].fillna(df['Company Tenure (In Months)'].median(), inplace=True)
@@ -88,7 +88,7 @@ df['Company Tenure (In Months)'].fillna(df['Company Tenure (In Months)'].median(
 # In[11]:
 
 
-# checking the result 
+# checking the result
 
 df.isnull().sum()
 
@@ -100,37 +100,40 @@ df.describe()
 
 
 # # Checking the redundant
-# Finding the redundant value in each and every column by finding unique values 
+# Finding the redundant value in each and every column by finding unique values
 
 
 # In[13]:
 
 
-# since we dont need employee id in the dataset so we can drop 
+# since we dont need employee id in the dataset so we can drop
 
-df=df.drop("Employee ID",axis=1)
+df = df.drop("Employee ID", axis=1)
 
 
 # In[14]:
 
 
-df['Education Level'].unique()  # since spelling error we have to replace 
+df['Education Level'].unique()  # since spelling error we have to replace
 
 
 # In[15]:
 
 
-# Data profiling 
+# Data profiling
 
-# Replace the wrong spelling with right spelling 
+# Replace the wrong spelling with right spelling
 
-df['Education Level']=df['Education Level'].replace(["Masterâ€™s Degree","Bachelorâ€™s Degree"],["Masters Degree","Bachelors Degree"])
+df['Education Level'] = df['Education Level'].replace(
+    ["Masterâ€™s Degree", "Bachelorâ€™s Degree"],
+    ["Masters Degree", "Bachelors Degree"]
+)
 
 
 # In[16]:
 
 
-df['Education Level'].unique() 
+df['Education Level'].unique()
 
 
 # In[17]:
@@ -160,20 +163,20 @@ df['Job Role'].unique()
 # In[21]:
 
 
-# applying loop for fast action 
+# applying loop for fast action
 
 cat_cols = df.select_dtypes(include='object').columns
 for col in cat_cols:
     print(f"{col}: {df[col].unique()}")
     print()
 
-# since ordinal data do change karinge encoding se, is 'Fair' ko change karna padega nahi to ordinal encoder me problem karega
+# since ordinal data, is 'Fair' ko change karna padega nahi to ordinal encoder me problem karega
 
 
 # In[22]:
 
 
-# Handling redundant values 
+# Handling redundant values
 
 df['Work-Life Balance'] = df['Work-Life Balance'].replace('Fair', 'Below Average')
 df['Company Reputation'] = df['Company Reputation'].replace('Fair', 'Poor')
@@ -188,17 +191,17 @@ df.head()
 # In[24]:
 
 
-# According to PDF we are doing train test split and then apply EDA on training and validation data 
+# According to PDF we are doing train test split and then apply EDA on training and validation data
 
 
-# # Train Test split 
+# # Train Test split
 
 
 # In[25]:
 
 
-x=df.drop("Attrition", axis=1)
-y=df["Attrition"]
+x = df.drop("Attrition", axis=1)
+y = df["Attrition"]
 
 
 # In[26]:
@@ -216,10 +219,10 @@ y.head()
 # In[28]:
 
 
-# Split the data into train and test 
-from sklearn.model_selection import train_test_split 
+# Split the data into train and test
+from sklearn.model_selection import train_test_split
 
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.30,random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.30, random_state=42)
 
 
 # In[29]:
@@ -240,7 +243,7 @@ x_test.head()
 # In[31]:
 
 
-sns.histplot(x_train['Age'],kde=True)
+sns.histplot(x_train['Age'], kde=True)
 plt.title('Distribution of Age')
 plt.show()
 
@@ -248,7 +251,7 @@ plt.show()
 # In[32]:
 
 
-sns.histplot(x_train['Years at Company'],kde=True)
+sns.histplot(x_train['Years at Company'], kde=True)
 plt.title('Distribution of Years at Company')
 plt.show()
 
@@ -256,7 +259,7 @@ plt.show()
 # In[33]:
 
 
-sns.histplot(x_train['Monthly Income'],kde=True)
+sns.histplot(x_train['Monthly Income'], kde=True)
 plt.title('Distribution of Monthly Income')
 plt.show()
 
@@ -264,7 +267,7 @@ plt.show()
 # In[34]:
 
 
-sns.histplot(x_train['Distance from Home'],kde=True)
+sns.histplot(x_train['Distance from Home'], kde=True)
 plt.title('Distribution of Distance from Home')
 plt.show()
 
@@ -272,7 +275,7 @@ plt.show()
 # In[35]:
 
 
-sns.histplot(x_train['Number of Promotions'],kde=True)
+sns.histplot(x_train['Number of Promotions'], kde=True)
 plt.title('Distribution of Number of Promotions')
 plt.show()
 
@@ -280,7 +283,7 @@ plt.show()
 # In[36]:
 
 
-sns.histplot(x_train['Number of Dependents'],kde=True)
+sns.histplot(x_train['Number of Dependents'], kde=True)
 plt.title('Distribution of Number of Dependents')
 plt.show()
 
@@ -288,24 +291,23 @@ plt.show()
 # In[37]:
 
 
-sns.histplot(x_train['Company Tenure (In Months)'],kde=True)
+sns.histplot(x_train['Company Tenure (In Months)'], kde=True)
 plt.title('Distribution of Company Tenure (In Months)')
 plt.show()
 
 
 # # Correlation heatmap
-   corr = x_train.corr(numeric_only=True)
-   x_train.corr(numeric_only=True)
 
 
 # In[38]:
 
 
-# check the multicollinearity 
+# check the multicollinearity
 
-plt.figure(figsize=(12,10))
-corr=x_train.corr()
-sns.heatmap(corr,annot=True)
+plt.figure(figsize=(12, 10))
+corr = x_train.corr(numeric_only=True)
+sns.heatmap(corr, annot=True)
+plt.show()
 
 
 # In[39]:
@@ -313,7 +315,7 @@ sns.heatmap(corr,annot=True)
 
 ## feature selection based on correlation
 
-x_train.corr()
+x_train.corr(numeric_only=True)
 
 
 # In[40]:
@@ -329,7 +331,7 @@ plt.show()
 # In[41]:
 
 
-# Almost balance hai 
+# Almost balance hai
 
 
 # # Bivariate analysis
@@ -338,106 +340,121 @@ plt.show()
 # In[42]:
 
 
-sns.countplot(train_df,x=y_train,hue=y_train)
+sns.countplot(data=train_df, x='Attrition', hue='Attrition')
 plt.title("Bar plot of Attrition")
+plt.show()
 
 
 # In[43]:
 
 
-sns.countplot(train_df,x=y_train,hue='Gender')
+sns.countplot(data=train_df, x='Attrition', hue='Gender')
 plt.title("Bar plot of Attrition on the basis of gender")
+plt.show()
 
 
 # In[44]:
 
 
-sns.countplot(train_df,x=y_train,hue='Job Role')
-plt.title("Bar plot of Attrition on the basis of Job Role ")
+sns.countplot(data=train_df, x='Attrition', hue='Job Role')
+plt.title("Bar plot of Attrition on the basis of Job Role")
+plt.show()
 
 
 # In[45]:
 
 
-sns.countplot(train_df,x=y_train,hue='Work-Life Balance')
+sns.countplot(data=train_df, x='Attrition', hue='Work-Life Balance')
 plt.title("Bar plot of Attrition on the basis of Work-Life Balance")
+plt.show()
 
 
 # In[46]:
 
 
-sns.countplot(train_df,x=y_train,hue='Job Satisfaction')
-plt.title("Bar plot of Attrition on the basis of Job Satisfaction ")
+sns.countplot(data=train_df, x='Attrition', hue='Job Satisfaction')
+plt.title("Bar plot of Attrition on the basis of Job Satisfaction")
+plt.show()
 
 
 # In[47]:
 
 
-sns.countplot(train_df,x=y_train,hue='Performance Rating')
+sns.countplot(data=train_df, x='Attrition', hue='Performance Rating')
 plt.title("Bar plot of Attrition on the basis of Performance Rating")
+plt.show()
 
 
 # In[48]:
 
 
-sns.countplot(train_df,x=y_train,hue='Job Level')
+sns.countplot(data=train_df, x='Attrition', hue='Job Level')
 plt.title("Bar plot of Attrition on the basis of Job Level")
+plt.show()
 
 
 # In[49]:
 
 
-sns.countplot(train_df,x=y_train,hue='Remote Work')
+sns.countplot(data=train_df, x='Attrition', hue='Remote Work')
 plt.title("Bar plot of Attrition on the basis of remote work")
+plt.show()
 
 
 # In[50]:
 
 
-sns.countplot(train_df,x=y_train,hue='Leadership Opportunities')
+sns.countplot(data=train_df, x='Attrition', hue='Leadership Opportunities')
 plt.title("Bar plot of Attrition on the basis of Leadership opportunities")
+plt.show()
 
 
 # In[51]:
 
 
-sns.countplot(train_df,x=y_train,hue='Company Size')
+sns.countplot(data=train_df, x='Attrition', hue='Company Size')
 plt.title("Bar plot of Attrition on the basis of Company Size")
+plt.show()
 
 
 # In[52]:
 
 
-sns.countplot(train_df,x=y_train,hue='Innovation Opportunities')
+sns.countplot(data=train_df, x='Attrition', hue='Innovation Opportunities')
 plt.title("Bar plot of Attrition on the basis of Innovation Opportunities")
+plt.show()
 
 
 # In[53]:
 
 
-sns.countplot(train_df,x=y_train,hue='Company Reputation')
+sns.countplot(data=train_df, x='Attrition', hue='Company Reputation')
 plt.title("Bar plot of Attrition on the basis of Company Reputation")
+plt.show()
 
 
 # In[54]:
 
 
-sns.countplot(train_df,x=y_train,hue='Employee Recognition')
+sns.countplot(data=train_df, x='Attrition', hue='Employee Recognition')
 plt.title("Bar plot of Attrition on the basis of Employee Recognition")
+plt.show()
 
 
 # In[55]:
 
 
-sns.countplot(train_df,x=y_train,hue='Education Level')
+sns.countplot(data=train_df, x='Attrition', hue='Education Level')
 plt.title("Bar plot of Attrition on the basis of Education Level")
+plt.show()
 
 
 # In[56]:
 
 
-sns.countplot(train_df,x=y_train,hue='Marital Status')
+sns.countplot(data=train_df, x='Attrition', hue='Marital Status')
 plt.title("Bar plot of Attrition on the basis of Marital Status")
+plt.show()
 
 
 # # Feature Engineering
@@ -446,7 +463,7 @@ plt.title("Bar plot of Attrition on the basis of Marital Status")
 # In[57]:
 
 
-# Ordinal encoding for columns in which order matters 
+# Ordinal encoding for columns in which order matters
 
 from sklearn.preprocessing import OrdinalEncoder
 OE = OrdinalEncoder()
@@ -466,9 +483,9 @@ Company_Reputation = ['Poor', 'Good', 'Excellent']
 Employee_Recognition = ['Low', 'Medium', 'High', 'Very High']
 
 encoder = OrdinalEncoder(categories=[Work_Life_Balance, Job_Satisfaction,
-                                      Performance_Rating, Education_Level,
-                                      Job_Level, Company_Size,
-                                      Company_Reputation, Employee_Recognition])
+                                     Performance_Rating, Education_Level,
+                                     Job_Level, Company_Size,
+                                     Company_Reputation, Employee_Recognition])
 
 cols = ['Work-Life Balance', 'Job Satisfaction', 'Performance Rating',
         'Education Level', 'Job Level', 'Company Size',
@@ -494,7 +511,7 @@ x_test.head()
 # In[61]:
 
 
-# Label encoding for columns in which order does not matter 
+# Label encoding for columns in which order does not matter
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
@@ -568,12 +585,13 @@ print(x_test.dtypes)
 # In[70]:
 
 
-# Feature Scaling 
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
 
-num_cols = ['Age', 'Years at Company', 'Monthly Income', 'Distance from Home', 'Number of Promotions','Number of Dependents','Company Tenure (In Months)']
+num_cols = ['Age', 'Years at Company', 'Monthly Income', 'Distance from Home',
+            'Number of Promotions', 'Number of Dependents', 'Company Tenure (In Months)']
 
 x_train[num_cols] = scaler.fit_transform(x_train[num_cols])
 x_test[num_cols] = scaler.transform(x_test[num_cols])
@@ -604,7 +622,7 @@ print(y_train.dtypes)
 x_train[num_cols]
 
 
-# # Model Building 
+# # Model Building
 
 
 # In[75]:
@@ -641,8 +659,6 @@ y_train.shape
 # In[80]:
 
 
-# this code is from chatgpt
-
 from sklearn.feature_selection import RFE
 
 rfe = RFE(estimator=LogisticRegression(max_iter=200), n_features_to_select=15)
@@ -671,7 +687,7 @@ lr.fit(x_train_rfe, y_train)
 # In[82]:
 
 
-# M ke value 
+# M ke value
 
 lr.coef_
 
@@ -679,7 +695,7 @@ lr.coef_
 # In[83]:
 
 
-# c ke value 
+# c ke value
 
 lr.intercept_
 
@@ -687,7 +703,7 @@ lr.intercept_
 # In[84]:
 
 
-y_pred=lr.predict(x_test_rfe)
+y_pred = lr.predict(x_test_rfe)
 
 
 # In[85]:
@@ -699,13 +715,13 @@ y_pred
 # In[86]:
 
 
-from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 
 
 # In[87]:
 
 
-print(confusion_matrix(y_test,y_pred))
+print(confusion_matrix(y_test, y_pred))
 
 
 # In[88]:
@@ -718,13 +734,13 @@ print('TP:', tp, 'TN:', tn, 'FP:', fp, 'FN:', fn)
 # In[89]:
 
 
-print(accuracy_score(y_test,y_pred))
+print(accuracy_score(y_test, y_pred))
 
 
 # In[90]:
 
 
-print(classification_report(y_test,y_pred))
+print(classification_report(y_test, y_pred))
 
 
 # In[91]:
@@ -746,7 +762,7 @@ print('Recall:', recall)
 
 from sklearn.metrics import roc_curve, roc_auc_score
 
-y_pred_prob = lr.predict_proba(x_test_rfe)[:,1]
+y_pred_prob = lr.predict_proba(x_test_rfe)[:, 1]
 fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
 
 plt.plot(fpr, tpr)
@@ -793,7 +809,11 @@ plt.show()
 # Conclusion:
 # 1. Job Level (Senior) and Remote Work are the strongest predictors of staying.
 # 2. Single Marital Status and High Overtime are the biggest threats to retention.
-# 3. Recommendation: HR should implement a "Work-Life Balance Audit" for Single employees in Mid-level roles, as they show the highest probability of leaving.
+# 3. Recommendation: HR should implement a "Work-Life Balance Audit" for Single employees
+#    in Mid-level roles, as they show the highest probability of leaving.
 
 
 # In[ ]:
+
+
+
